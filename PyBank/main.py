70e39865b038180976668d009e253e941f253254
mvh267 <matthohner@gmail.com>
2023-11-14 16:21:38 -0600

    this is total months and profit

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+budget_csvpath = os.path.join("Resources", "budget_data.csv")
+
+months = []
+profit = []
+
+
+count_months = 0
+net_profit = 0
+
+
+with open(budget_csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile)
+    
+    for row in csvreader:
+        count_months = count_months + int(row[1])
+        net_profit = net_profit + int(row[1])
+        
+        
+
+print(f"Total Months: {count_months}")
+print(f"Total_Profit: {net_profit}")
